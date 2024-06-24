b8f074cc989fb210c9c2f617b67cda88cab744ce
wasifafaque8 <afaqueplayer4@gmail.com>
2024-06-24 11:52:52 +0530

    "Added CORS and cookie-parser middleware to Express app, updated .env file, and modified package.json and package-lock.json files to include new dependencies."

--- src/index.js
+++ src/index.js
@@ -10,6 +10,18 @@ dotenv.config({
 })
 
 connectDB()
+.then(() =>{
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Server is running at port : $ 
+            {process.env.PORT}`);
+
+    })
+
+})
+.catch((err) => {
+    console.log("Mongo db connect failed!!",err)
+
+})
 
 
 
